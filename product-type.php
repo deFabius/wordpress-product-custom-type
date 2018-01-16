b0af374ff6a84553378664d4d4af103754e29b8f
deFabius <MayDay@01>
2018-01-16 13:07:34 +0000

    Add custom taxonomies to custom products and remove default categories.

--- product-type.php
+++ product-type.php
@@ -38,7 +38,7 @@ function product_post_type_register() {
 				),
 			'has_archive' => true,
 			'hierarchical' => false,
-			'taxonomies' => array('category'),
+			'taxonomies' => array('product_category', 'brand'),
 			'public' => true,
 			'menu_position' => 5,
 			'menu_icon' => 'dashicons-media-interactive',
@@ -53,6 +53,69 @@ function product_post_type_register() {
 }
 
 add_action( 'init', 'product_post_type_register' );
+
+add_action( 'init', 'create_custom_product_taxonomies', 0 );
+
+// create two taxonomies, genres and writers for the post type "book"
+function create_custom_product_taxonomies() {
+	// Add new taxonomy, make it hierarchical (like categories)
+	$labels = array(
+		'name'              => _x( 'Product Categories', 'taxonomy general name', 'textdomain' ),
+		'singular_name'     => _x( 'Product Category', 'taxonomy singular name', 'textdomain' ),
+		'search_items'      => __( 'Search Product Categories', 'textdomain' ),
+		'all_items'         => __( 'All Product Categories', 'textdomain' ),
+		'parent_item'       => __( 'Parent Product Category', 'textdomain' ),
+		'parent_item_colon' => __( 'Parent Product Category:', 'textdomain' ),
+		'edit_item'         => __( 'Edit Product Category', 'textdomain' ),
+		'update_item'       => __( 'Update Product Category', 'textdomain' ),
+		'add_new_item'      => __( 'Add New Product Category', 'textdomain' ),
+		'new_item_name'     => __( 'New Product Category Name', 'textdomain' ),
+		'menu_name'         => __( 'Products Categories', 'textdomain' ),
+	);
+
+	$args = array(
+		'hierarchical'      => true,
+		'labels'            => $labels,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'rewrite'           => array( 'slug' => 'product_category' ),
+	);
+
+	register_taxonomy( 'product_category', array( 'product' ), $args );
+
+	// Add new taxonomy, NOT hierarchical (like tags)
+	$labels = array(
+		'name'                       => _x( 'Brands', 'taxonomy general name', 'textdomain' ),
+		'singular_name'              => _x( 'Brand', 'taxonomy singular name', 'textdomain' ),
+		'search_items'               => __( 'Search Brands', 'textdomain' ),
+		'popular_items'              => __( 'Popular Brands', 'textdomain' ),
+		'all_items'                  => __( 'All Brands', 'textdomain' ),
+		'parent_item'                => null,
+		'parent_item_colon'          => null,
+		'edit_item'                  => __( 'Edit Brand', 'textdomain' ),
+		'update_item'                => __( 'Update Brand', 'textdomain' ),
+		'add_new_item'               => __( 'Add New Brand', 'textdomain' ),
+		'new_item_name'              => __( 'New Brand Name', 'textdomain' ),
+		'separate_items_with_commas' => __( 'Separate brands with commas', 'textdomain' ),
+		'add_or_remove_items'        => __( 'Add or remove brands', 'textdomain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used brands', 'textdomain' ),
+		'not_found'                  => __( 'No brands found.', 'textdomain' ),
+		'menu_name'                  => __( 'Brands', 'textdomain' ),
+	);
+
+	$args = array(
+		'hierarchical'          => false,
+		'labels'                => $labels,
+		'show_ui'               => true,
+		'show_admin_column'     => true,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var'             => true,
+		'rewrite'               => array( 'slug' => 'brand' ),
+	);
+
+	register_taxonomy( 'brand', 'product', $args );
+}
 
 function product_fields (){
 	global $post;
